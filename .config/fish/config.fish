65494d91ead95934b2ae19b006db1d2bd3d30b91
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2023-04-03 17:44:39 +0200

    fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+    set_color green
+    echo -n "W"
+    set_color yellow
+    echo -n "e"
+    set_color cyan
+    echo -n "l"
+    set_color red
+    echo -n "c"
+    set_color purple
+    echo -n "o"
+    set_color blue
+    echo -n "m"
+    set_color brblue
+    echo -n "e"
+    set_color normal
+    echo " to everyone !"
+    echo ""
+end
+
+function fish_prompt
+    echo -n "[ " 
+    set_color purple
+    echo -n "$USER"
+    set_color normal
+    echo "@$(/bin/hostname) ]"
+    echo -n "\$ "
+end

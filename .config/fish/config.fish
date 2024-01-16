0d4f1dba66d46443a273d2757b3e85f68ecf5a33
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2024-01-16 18:57:32 +0100

    1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,38 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-function fish_greeting
-    set_color green
-    echo -n "W"
-    set_color yellow
-    echo -n "e"
-    set_color cyan
-    echo -n "l"
-    set_color red
-    echo -n "c"
-    set_color purple
-    echo -n "o"
-    set_color blue
-    echo -n "m"
-    set_color brblue
-    echo -n "e"
-    set_color normal
-    echo " to everyone !"
-end
-
-function fish_prompt
-    echo ""
-    echo -n "[ " 
-    set_color purple
-    echo -n "$USER"
-    set_color normal
-    echo "@$(/bin/hostname):$(pwd) ]"
-    echo -n "\$ "
-end
-
-# convenient alisases
-alias gcom="git add --all; git commit -S"
-alias hx="helix"
-alias yadmcom="yadm commit -S"eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
